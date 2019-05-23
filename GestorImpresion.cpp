931292ee7655ce8c2fe03ab8adfe7d612643e484
fernando1612 <fernando.patricio.maceda@gmail.com>
2019-05-22 22:41:38 -0500

    primer commit

--- GestorImpresion.cpp
+++ GestorImpresion.cpp
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
+
+#define USUARIO "fernando"
+#define CLAVE "maceda"
+
+#define LONGITUD 80
 
 //DECLARACION DE PROTOTIPOS DE FUNCIONES
 int bienvenida();
 int menu(int);
+int login();
 
 //Fin de declaracion de prototipos de funciones
 
@@ -12,6 +19,7 @@ int main()
 {
 	int op; 
 	int opmenu;
+	login();
 	bienvenida();
 	op = menu (opmenu);
 	printf("%i", op);
@@ -33,17 +41,73 @@ int main()
 			return 0;
 			break;
 		default:
-			printf("Opcion no valida, por favor, ingrese un valor valido ")
+			printf("Opcion no valida, por favor, ingrese un valor valido ");
 	}
 	return 0;
 }
 
+int login(){
+	char usuario[LONGITUD + 1];
+    char clave[LONGITUD + 1];
+    int intento = 0;
+    int ingresa = 0;
+    char caracter;
+    int i = 0;
+    
+    do {
+        i = 0;
+        system("cls");
+        printf("\n\t\t\tINICIO DE SESION\n");
+        printf("\t\t\t---------------\n");
+        printf("\n\tUSUARIO: ");
+        gets(usuario);
+        printf("\tCLAVE: ");
+        while (caracter = getch()) {
+            if (caracter == 13) {
+                clave[i] = '\0';
+                break;
+                
+            } else if (caracter == 8) {
+                if (i > 0) {
+                    i--;
+                    printf("\b \b");
+                }
+                
+            } else {
+            	               if (i < LONGITUD) {
+                    printf("*");
+                    clave[i] = caracter;
+                    i++;
+                }
+            }
+        }
+        
+        if (strcmp(usuario, USUARIO) == 0 && strcmp(clave, CLAVE) == 0) {
+            ingresa = 1;
+            
+        } else {
+            printf("\n\tUsuario y/o clave son incorrectos\n");
+            intento++;
+            getchar();
+        }
+        
+    } while (intento < 3 && ingresa == 0);
+    
+    if (ingresa == 1) {
+        printf("\n\tBienvenido al Sistema\n");
+        
+    } else {
+        printf("\n\tHa sobrepasado el numero maximo de intentos permitidos\n");
+    }
+
+    return 0;
+}
 int bienvenida()
 {
-	char nombre[20];
-	printf("Por favor, ingrese su nombre\n");
-	scanf("%s", &nombre);
-	printf("Bienvenido %s, por favor, elija alguna de las siguientes opciones:\n", nombre);
+//	char nombre[20];
+//	printf("Por favor, ingrese su nombre\n");
+//	scanf("%s", &nombre);
+	printf("Bienvenido %s, por favor, elija alguna de las siguientes opciones:\n", USUARIO);
 	
 	return 0;
 }
